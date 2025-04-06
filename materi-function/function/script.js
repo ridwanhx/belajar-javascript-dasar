69b4d5d53ace3557a8743948fc2faace395aed8d
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-04-06 22:33:20 +0700

    Eps. 25 - Belajar Javascript Dasar : Function

---
+++ materi-function/function/script.js
@@ -0,0 +1,15 @@
+// # Function
+// Function adalah blok kode program yang akan berjalan saat kita panggil
+// Sebelumnya kita sudah menggunakan function writeln() untuk menampilkan tulisan di html
+// Untuk membuat function, kita bisaa menggunakan kata kunci function, lalu diikuti dengan nama function, kurung (), dan kemudian diakhiri dengna blok kurung kurawal {}
+// Kita bisa memanggil function dengna menggunakan nama function lalu diikuti dengna kurung ()
+// Di bahasa pemograman lain, function juga disebut dengan method
+// penulisannya biasa menggunakan camel case
+// implementasi
+function sayHelloWorld() {
+    // document.writeln('Hello World!');    // kebetulan memang tadinya kita akan melakukan ini seperti yang ada di video pembelajaran, namun karena kita sudah terlanjur menuliskan keseluruhan kode program menggunakan javascript, maka kita coba buat sedikit lebih beda.
+    console.info('Hello World!');
+}
+// memanggil function
+sayHelloWorld();
+sayHelloWorld();    // boleh dipanggil berkali-kali
